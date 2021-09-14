5edf9065806d9bf9bfec62f91fde82168f811758
λ <richard.sxs@alibaba-inc.com>
2021-09-15 00:49:34 +0800

    支持markdown

--- init.el
+++ init.el
@@ -322,6 +322,19 @@
   :defer t
   )
 
+(use-package markdown-mode
+  :ensure t
+  :defer t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
+(use-package org
+  :ensure t
+)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
